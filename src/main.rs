4e5cc0627d7e7a15487afa50d4ee7daed59d4e33
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-28 00:04:02 +0200

    Add Rust program for compression

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use flate2::write::DeflateEncoder;
+use flate2::Compression;
+use std::fs::File;
+use std::io::{self, Read, Write};
+
+fn main() -> io::Result<()> {
+    let mut input_file = File::open("test.txt")?;
+    let mut output_file = File::create("output.deflate")?;
+
+    let mut compressor = DeflateEncoder::new(&mut output_file, Compression::best());
+    let mut buffer = [0; 1024];
+    loop {
+        let bytes_read = input_file.read(&mut buffer)?;
+        if bytes_read == 0 {
+            break;
+        }
+        compressor.write_all(&buffer[..bytes_read])?;
+    }
+    compressor.finish()?;
+    Ok(())
 }
